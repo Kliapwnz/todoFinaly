53b86f4397eff58352bd429f7b838862a7813a9d
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-20 13:42:55 +0300

    Handlers

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,8 +1,29 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+type PropsType={
+    addTask: (todolistID: string, title: string) => void
+    todolistID: string
+}
 
-export const AddItemForm = () => {
+export const AddItemForm = (props:PropsType) => {
     let [title, setTitle] = useState("")
     let [error, setError] = useState<string | null>(null)
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.currentTarget.value)
+    }
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+        setError(null);
+        if (e.charCode === 13) {
+            addTask();
+        }
+    }
+    const addTask = () => {
+        if (title.trim() !== "") {
+            props.addTask(props.todolistID, title.trim());
+            setTitle("");
+        } else {
+            setError("Title is required");
+        }
+    }
     return (
         <div>
             <input value={title}
