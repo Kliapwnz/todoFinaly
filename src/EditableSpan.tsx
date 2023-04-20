2b0c734589b871cecc9ba3a05f4b68ff5ad03f91
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-20 16:15:04 +0300

    editHandler

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -6,9 +6,12 @@ type PropsType = {
 
 export const EditableSpan = (props: PropsType) => {
     const [edit, setEdit] = useState(false)
+    const editHandler =()=> {
+        setEdit(true)
+    }
     return (
         edit ? <input value={props.title}/> :
-        <span>{props.title}</span>
+        <span onDoubleClick={editHandler}>{props.title}</span>
     );
 };
 
