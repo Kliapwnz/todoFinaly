209e3bcdcaaeb0a9034994d17055cb5918cdd267
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-20 16:11:54 +0300

    EditableSpan.tsx

--- src/EditableSpan.tsx
+++ src/EditableSpan.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 type PropsType = {
     title: string
 }
 
 export const EditableSpan = (props: PropsType) => {
+    const [edit, setEdit] = useState(false)
     return (
         <span>{props.title}</span>
     );
