7b86631385b35067f8ded2a08d54322f91319500
Evgeniy Klimovch <kliasanik@gmail.com>
2023-04-24 16:00:46 +0300

    fontWeight

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -46,6 +46,7 @@ export function Todolist(props: PropsType) {
         <Typography
             variant={"h5"}
             align={"center"}
+            fontWeight={"bold"}
         >
             <EditableSpan oldTitle={props.title} callBack={updateTodoHandler}/>
             <IconButton
